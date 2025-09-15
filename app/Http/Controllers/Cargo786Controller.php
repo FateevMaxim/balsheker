b11f5c544598e22340eada923a4a344b4f1e41dd
maxim.f <maxim.f@rocketfirm.com>
2025-09-15 18:12:48 +0500

    update tracklist logic to use updateOrCreate

--- app/Http/Controllers/Cargo786Controller.php
+++ app/Http/Controllers/Cargo786Controller.php
@@ -243,13 +243,16 @@ class Cargo786Controller extends Controller
                 'freight_cost' => $validated['freight_cost']
             ]);
 
-            TrackList::query()->create([
-                'track_code' => $validated['express_sn'],
-                'to_china' => date(now()),
-                'status' => 'Получено в Китае',
-                'reg_china' => 1,
-                'created_at' => date(now()),
-            ]);
+            TrackList::query()->updateOrCreate(
+                [
+                    'track_code' => $validated['express_sn']
+                ],
+                [
+                    'to_china' => now(),
+                    'status' => 'Получено в Китае',
+                    'reg_china' => 1,
+                ]
+            );
 
             return response()->json([
                 'success' => true,
